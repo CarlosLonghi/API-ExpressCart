2ea57ab18a728abf8bd021a0474cfd2e5c2fa4c8
CarlosLonghi <carloslonghi.cl@gmail.com>
2025-06-24 23:52:46 -0300

    Initial Commit

---
+++ src/main/java/br/com/expresscart/ExpressCartApplication.java
@@ -0,0 +1,13 @@
+package br.com.expresscart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpressCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpressCartApplication.class, args);
+	}
+
+}
