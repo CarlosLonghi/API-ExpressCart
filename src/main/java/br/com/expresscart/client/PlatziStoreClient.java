a7af249c55f4b9cf8cf0aabe2da4616cefae63f4
CarlosLonghi <carloslonghi.cl@gmail.com>
2025-06-25 21:14:38 -0300

    feat: add `client` for connect to external API `Platzi Fake Store` using `Cloud OpenFeign`

---
+++ src/main/java/br/com/expresscart/client/PlatziStoreClient.java
@@ -0,0 +1,19 @@
+package br.com.expresscart.client;
+
+import br.com.expresscart.client.response.PlatziProductResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.Optional;
+
+@FeignClient(name = "platzi-store-client", url = "${expresscart.client.platzi}")
+public interface PlatziStoreClient {
+
+    @GetMapping("/products")
+    List<PlatziProductResponse> getAllProducts();
+
+    @GetMapping("products/{id}")
+    Optional<PlatziProductResponse> getProductById(@PathVariable Long id);
+}
