9d04fd9df67ff273c4143b0e45039555647bf5f5
carlos <carloslonghi.cl@gmail.com>
2025-07-12 13:54:34 -0300

    feat: add `CustomErrorDecoder` and config to `PlatziStoreClient`

--- src/main/java/br/com/expresscart/client/PlatziStoreClient.java
+++ src/main/java/br/com/expresscart/client/PlatziStoreClient.java
@@ -1,13 +1,18 @@
 package br.com.expresscart.client;
 
 import br.com.expresscart.client.response.PlatziProductResponse;
+import br.com.expresscart.exceptions.CustomErrorDecoder;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "platzi-store-client", url = "${expresscart.client.platzi}")
+@FeignClient(
+        name = "platzi-store-client",
+        url = "${expresscart.client.platzi}",
+        configuration = {CustomErrorDecoder.class}
+)
 public interface PlatziStoreClient {
 
     @GetMapping("/products")
