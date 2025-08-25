7bd7211683dc7fe6d0f2ba2da117c0fb41999b07
carlos <carloslonghi.cl@gmail.com>
2025-08-25 12:06:56 -0300

    fix(validation): improve request validation in CartRequest, PaymentRequest and ProductRequest

--- src/main/java/br/com/expresscart/controller/request/ProductRequest.java
+++ src/main/java/br/com/expresscart/controller/request/ProductRequest.java
@@ -1,7 +1,16 @@
 package br.com.expresscart.controller.request;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
 public record ProductRequest(
+        @NotNull(message = "Informe o ID do Produto")
         Long id,
+
+        @NotNull(message = "Informe a quantidade que deseja adicionar ao Carrinho")
+        @Min(value = 1, message = "A quantidade deve ser maior que 0")
+        @Max(value = 100, message = "A quantidade máxima permitida é 100")
         Integer quantity
 ) {
 }
